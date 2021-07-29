bce5d03ad52a47becb27a7e2b73a54731a616d33
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-07-29 10:38:12 +0700

    success make portofolio list and about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import PortofolioList from "./components/PortofolioList";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <PortofolioList />
+      <About />
     </>
   );
 }
